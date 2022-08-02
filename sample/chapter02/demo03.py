8c1a1dd3a141e6b292c4dcfa26da0ee6e71f6884
jiweiwang <jiwei_it@163.com>
2022-08-02 17:11:45 +0800

    "python中魔法函数/demo03.py/len函数的重要性"

---
+++ sample/chapter02/demo03.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python3.9
+# encoding: utf-8
+# author: nextHeartbeat
+# software: pycharm
+# file: demo03.py
+# time: 2022/8/2 11:13
+"""
+    len函数的特殊性:
+        1. cpython 中C 编写, 直接读取, 无需便利, 内置优化, 魔法方法中一大部分都内置优化过
+"""
