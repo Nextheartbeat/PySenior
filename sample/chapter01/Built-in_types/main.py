54ad6806a1fba594e694ad7200a5d8829e1a6f06
jiweiwang <jiwei_it@163.com>
2022-08-02 15:00:46 +0800

    "python中一切皆对象/常见的内置类型/main.py"

---
+++ sample/chapter01/Built-in_types/main.py
@@ -0,0 +1,36 @@
+# !/usr/bin/env python3.9
+# encoding: utf-8
+# author: nextHeartbeat
+# software: pycharm
+# file: all_is_object.py
+# time: 2022/8/2 11:13
+"""对象的三大特征: 身份(内存地址), 类型, 值"""
+
+"""None(全局只有一个)"""
+a = None
+b = None
+print(id(a) == id(b))
+
+"""数值: int, float, complex(复数), bool"""
+
+"""迭代类型"""
+
+"""序列类型: list, bytes, bytearray, memoryView(二进制序列), range, tuple, str, array"""
+
+"""映射(dict)"""
+
+"""集合: set, frozenset(不可修改的集合)"""
+
+"""上下文管理类型(with)"""
+
+"""其他:
+    1. 模块类型
+    2. class和实例
+    3. 函数类型
+    4. 方法类型
+    5. 代码类型
+    6. object对象
+    7. type类型
+    8. elipsis类型(省略号)
+    9. notimplemented类型
+"""
