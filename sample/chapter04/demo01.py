984e817a53550cbc3b6fd074459a79efcdc11ff6
wangjiwei <jiwei_it@163.com>
2022-09-07 13:08:26 +0800

    "python 自定义序列类/序列类型的分类"

---
+++ sample/chapter04/demo01.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3.9
+# encoding: utf-8
+# author: nextHeartbeat
+# software: pycharm
+# file: demo01.py
+# time: 2022/9/7 10:51
+"""
+    序列类型的分类
+        1. 容器序列: list, tuple, deque
+        2. 扁平序列: str, bytes, bytearray, array.array
+        3. 可变序列: list, deque, bytearray, array
+        4. 不可变序列: str, tuple, bytes
+"""
+from collections import abc
