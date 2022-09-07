040e61e9e808dc1260b2aa29a05f2cecc401192c
wangjiwei <jiwei_it@163.com>
2022-09-07 13:08:38 +0800

    "python 自定义序列类/序列的 abc 继承关系"

---
+++ sample/chapter04/demo02.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python3.9
+# encoding: utf-8
+# author: nextHeartbeat
+# software: pycharm
+# file: demo02.py
+# time: 2022/9/7 10:51
+"""
+    序列的 abc 继承关系
+        "Sequence",
+        "MutableSequence",
+"""
+from collections import abc
